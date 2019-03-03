f93ced2aa22a792e36e82db654b26d209352f81d
Rafyrt <rafyrt@gmail.com>
2019-03-02 19:32:12 -0800

    Se le agrego funcion Crop y junta las 4 imagenes en 1

--- ejc2Test.py
+++ ejc2Test.py
@@ -1,5 +1,3 @@
-
-
 import cv2
 import numpy as np
 
@@ -20,20 +18,27 @@ def FeatureMatching(img2, img1):
         src = np.float32([ kp1[m.queryIdx].pt for m in matches[:,0] ]).reshape(-1,1,2)
         dst = np.float32([ kp2[m.trainIdx].pt for m in matches[:,0] ]).reshape(-1,1,2)
         H, masked = cv2.findHomography(src, dst, cv2.RANSAC, 5.0)
-        print(H)
     else:
         pass
     dst = cv2.warpPerspective(img1,H,(img2.shape[1] + img1.shape[1], img2.shape[0]))
     dst[0:img1.shape[0], 0:img2.shape[1]] = img2
-    return dst
+    return CropBlackSpace(dst)
+
+def CropBlackSpace(img):
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    mask = gray>0
+    gray = gray[np.ix_(mask.any(1),mask.any(0))]
+    return img[:gray.shape[0],:gray.shape[1]]
+
 img1 = cv2.imread('Database/fig1.png')
 img2 = cv2.imread('Database/fig2.png')
 img3 = cv2.imread('Database/fig3.png')
 img4 = cv2.imread('Database/fig4.png')
 out1_2 = FeatureMatching(img1,img2)
 out3_4 = FeatureMatching(img3,img4)
-cv2.imwrite('out1_2.png',out1_2)
-cv2.imwrite('out3_4.png',out3_4)
+out = FeatureMatching(out1_2,out3_4)
+cv2.imwrite('out.png',out)
+##cv2.imwrite('out3_4.png',out3_4)
 # cv2.imshow('out2_3',out2_3)
 # cv2.imshow('out3_4',out3_4)
 
