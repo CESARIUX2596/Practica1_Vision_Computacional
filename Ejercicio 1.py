abe7aca2d2186baa6d01c34d77666c37d8cddc56
CesarTorres2596 <37921177+CesarTorres2596@users.noreply.github.com>
2019-03-02 16:37:52 -0800

    Changes to exc2

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -7,11 +7,12 @@ while (cap.isOpened()):
     try:
 
         ret, frame = cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        blured = cv2.GaussianBlur(gray,(13,13),10)
-        dst = cv2.cornerHarris(blured,2,7,0.05)
+        frame = cv2.frame(frame, (640, 480)) 
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # blured = cv2.GaussianBlur(gray,(13,13),10)
+        # dst = cv2.cornerHarris(blured,2,7,0.05)
         # dst = cv2.dilate(dst,None)
-        frame[dst>0.01*dst.max()]=[0,255,0]
+        # frame[dst>0.01*dst.max()]=[0,255,0]
         # cv2.imshow('blured',blured)
         cv2.imshow('frame',frame)
         # cv2.imshow('gray',gray)
---
+++ ejc2Test.py
@@ -0,0 +1,41 @@
+
+
+import cv2
+import numpy as np
+
+def FeatureMatching(img2, img1):
+    gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    sift = cv2.xfeatures2d.SIFT_create()
+    kp1, des1 = sift.detectAndCompute(gray1, None)
+    kp2, des2 = sift.detectAndCompute(gray2, None)
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des1,des2, k=2) 
+    good = []
+    for m in matches:
+        if m[0].distance < 0.5*m[1].distance:         
+            good.append(m)
+    matches = np.asarray(good)
+    if len(matches[:,0]) >= 4:
+        src = np.float32([ kp1[m.queryIdx].pt for m in matches[:,0] ]).reshape(-1,1,2)
+        dst = np.float32([ kp2[m.trainIdx].pt for m in matches[:,0] ]).reshape(-1,1,2)
+        H, masked = cv2.findHomography(src, dst, cv2.RANSAC, 5.0)
+        print(H)
+    else:
+        pass
+    dst = cv2.warpPerspective(img1,H,(img2.shape[1] + img1.shape[1], img2.shape[0]))
+    dst[0:img1.shape[0], 0:img2.shape[1]] = img2
+    return dst
+img1 = cv2.imread('Database/fig1.png')
+img2 = cv2.imread('Database/fig2.png')
+img3 = cv2.imread('Database/fig3.png')
+img4 = cv2.imread('Database/fig4.png')
+out1_2 = FeatureMatching(img1,img2)
+out3_4 = FeatureMatching(img3,img4)
+cv2.imwrite('out1_2.png',out1_2)
+cv2.imwrite('out3_4.png',out3_4)
+# cv2.imshow('out2_3',out2_3)
+# cv2.imshow('out3_4',out3_4)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
