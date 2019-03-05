6ddc2466a5b76a1b68e858246ea7b94f036a23b1
CesarTorres2596 <37921177+CesarTorres2596@users.noreply.github.com>
2019-03-04 21:51:48 -0800

    EJercicio2 con el codigo completo

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -1,29 +1,44 @@
 import cv2
 import numpy as np
 
-def FeatureMatching(img1, img2):
+def FeatureMatching(img2, img1):
     gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
     sift = cv2.xfeatures2d.SIFT_create()
-    kp1, des1 = sift.detectAndCompute(img1, None)
-    kp2, des2 = sift.detectAndCompute(img2, None)
-    bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
-    matches = bf.match(des1,des2)
-    matches = sorted(matches, key = lambda x:x.distance)
-    img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:30], None, flags = 2)
-    return img3, kp1, des1
+    kp1, des1 = sift.detectAndCompute(gray1, None)
+    kp2, des2 = sift.detectAndCompute(gray2, None)
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des1,des2, k=2) 
+    good = []
+    for m in matches:
+        if m[0].distance < 0.5*m[1].distance:         
+            good.append(m)
+    matches = np.asarray(good)
+    if len(matches[:,0]) >= 4:
+        src = np.float32([ kp1[m.queryIdx].pt for m in matches[:,0] ]).reshape(-1,1,2)
+        dst = np.float32([ kp2[m.trainIdx].pt for m in matches[:,0] ]).reshape(-1,1,2)
+        H, masked = cv2.findHomography(src, dst, cv2.RANSAC, 5.0)
+    else:
+        pass
+    dst = cv2.warpPerspective(img1,H,(img2.shape[1] + img1.shape[1], img2.shape[0]))
+    dst[0:img1.shape[0], 0:img2.shape[1]-10] = img2[:,:img2.shape[1]-10]
+    return CropBlackSpace(dst)
+
+def CropBlackSpace(img):
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    mask = gray>0
+    gray = gray[np.ix_(mask.any(1),mask.any(0))]
+    return img[:gray.shape[0],:gray.shape[1]]
 
 img1 = cv2.imread('Database/fig1.png')
 img2 = cv2.imread('Database/fig2.png')
 img3 = cv2.imread('Database/fig3.png')
 img4 = cv2.imread('Database/fig4.png')
-
-out1_2,kp1,des1 = FeatureMatching(img1,img2)
-print (kp1)
-print(des1)
-# out2_3 = FeatureMatching(img2,img3)
-# out3_4 = FeatureMatching(img3,img4)
-cv2.imshow('out1_2',out1_2)
+out1_2 = FeatureMatching(img1,img2)
+out3_4 = FeatureMatching(img3,img4)
+out = FeatureMatching(out1_2,out3_4)
+cv2.imwrite('out.png',out)
+##cv2.imwrite('out3_4.png',out3_4)
 # cv2.imshow('out2_3',out2_3)
 # cv2.imshow('out3_4',out3_4)
 
